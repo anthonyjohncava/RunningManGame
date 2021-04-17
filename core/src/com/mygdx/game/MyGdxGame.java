66ed3cef4a36eea076549b26cc38dbef101e2eba
Anthony John Cava <cavay010@mymail.unisa.edu.au>
2021-04-17 11:06:27 +0930

    Added GameScreen and MenuScreen class

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,48 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class MyGdxGame extends Game implements ApplicationListener {
+
+	// The class with the menu
+	public static MenuScreen menuScreen;
+
+	// The class with the game
+	public static GameScreen gameScreen;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.app.log("MyGdxGame: ", "create");
+		gameScreen = new GameScreen(this);
+		menuScreen = new MenuScreen(this);
+		Gdx.app.log("MyGdxGame: ", "about to change screen to menuScreen");
+		// Change screens to the menu
+		setScreen(menuScreen);
+		Gdx.app.log("MyGdxGame: ", "changed screen to menuScreen");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+	@Override
+	public void pause() {
+		super.pause();
+	}
+	@Override
+	public void resume() {
+		super.resume();
 	}
 }
