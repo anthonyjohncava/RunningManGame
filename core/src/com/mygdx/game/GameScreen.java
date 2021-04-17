fa8b049f053bcb123b9127bc5b0b904b1a1b43e9
Anthony John Cava <cavay010@mymail.unisa.edu.au>
2021-04-17 12:19:46 +0930

    Added animation to make character run in place

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -12,56 +12,63 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class GameScreen implements Screen {
     MyGdxGame game;
     private Music backgroundMusic;
-    SpriteBatch spriteBatch;
+    SpriteBatch spriteBatch;                                // Spritebatch for rendering
 
-    // Character running
-    Texture runningSheet;
-    TextureRegion[] runFrames;
-    // Rows and cols of the running spritesheet
-    private static final int FRAME_COLS = 4;
-    private static final int FRAME_ROWS = 2;
+    // Main character variables
+    Texture runningSheet;                                   // Texture to hold the spritesheet
+    TextureRegion[] runFrames;                              // Texture array for the running frames
+    private static final int FRAME_COLS = 4;                // Number of columns of the running spritesheet
+    private static final int FRAME_ROWS = 2;                // Number of rows of the running spritesheet
+    private static final int character_posX = 10;           // Position X of the character on the screen
+    private static final int character_posY =10;            // Position Y of the character on the screen
+    private static final int character_height = 300;        // Height of the character
+    private static final int character_width = 300;         // Width of the character
+
     // Variables for the running animation
-//    Animation runAnimation;		// This will store the array containing all of the frames, which is the walkFrames. It will also have the defined duration (in seconds) for each frame
-//    TextureRegion currentFrame;
-    // Position of the character
-    private static final int character_posX = 10;
-    private static final int character_posY =10;
-    // Size of the character
-    private static final int character_height = 300;
-    private static final int character_width = 300;
+    Animation runAnimation;		                            // Stores the array containing all of runFrames. It will also have the defined duration (in seconds) for each frame
+    TextureRegion currentFrame;                             // Current frame to display
 
+    float stateTime;                                        // The time the program has been running.
 
 //    int frameIndex;
-//    float stateTime;
 
     public GameScreen(MyGdxGame game) {
         this.game = game;
     }
 
     public void create() {
-        spriteBatch = new SpriteBatch();
-
-        // background music
+        // Loaded the background music
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("background_music.mp3"));
-        backgroundMusic.setLooping(true);
-        backgroundMusic.play();
 
-        // Running character---------------
+        // Running character------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        // Loaded the runnning spritesheet
         runningSheet = new Texture(Gdx.files.internal("assets/running.png"));
+
         // Creates a 2D array of the given spritesheet
         TextureRegion[][] temp = TextureRegion.split(runningSheet, runningSheet.getWidth() / FRAME_COLS, runningSheet.getHeight() / FRAME_ROWS);
-        // We want the above code to only be a single array. This sets the size of the array.
+
+        // Sets the size of the array for the above code to only be a 1D array
         runFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        // Store each texture on the runFrames array.
+
+        // Transfers each texture on the temp 2D array to the 1D runFrames array.
         int index = 0;
         for (int i = 0; i < FRAME_ROWS; i++) {
             for (int j = 0; j < FRAME_COLS; j++) {
                 runFrames[index++] = temp[i][j];    // i++ is post increment.
             }
-//            // Animation setup
-//            runAnimation = new Animation(0.4f, runFrames);
-//            stateTime = 0.0f;
         }
+
+        // Sets the runFrames TextureTesgion into an Animation object, with a framerate set to 0.033, which is 30 frames per second.
+        runAnimation = new Animation(0.033f, runFrames);
+        // Running character------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+        // Initialises the stateTime
+        stateTime = 0.0f;
+
+        // initialises the spriteBatch
+        spriteBatch = new SpriteBatch();
     }
 
     @Override
@@ -75,12 +82,19 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-//        // Running animation -----------------
-//        stateTime += Gdx.graphics.getDeltaTime();
-//        currentFrame = (TextureRegion) runAnimation.getKeyFrame(stateTime, true);
-        // Draw character
+        // Starts background music
+        backgroundMusic.setLooping(true);
+        backgroundMusic.play();
+
+        // Updates the stateTime using the deltaTime (to have the same time across all devices with different processors)
+        stateTime += Gdx.graphics.getDeltaTime();
+
+        // Gets the currentFrame of the running animation
+        currentFrame = (TextureRegion) runAnimation.getKeyFrame(stateTime, true);
+
+        // Draws the character's currentframe on the screen, with a set position, and the size of the character.
         spriteBatch.begin();
-        spriteBatch.draw(runFrames[0], character_posX, character_posY, character_width, character_height);
+        spriteBatch.draw(currentFrame, character_posX, character_posY, character_width, character_height);
         spriteBatch.end();
     }
 
